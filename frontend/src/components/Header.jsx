9d054de299e52c351d499f9853fda5dc27ffa1fe
Cagla aratli <caglaratlii@gmail.com>
2024-02-28 16:18:11 +0300

    Header component completed  : icon added, styles created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
           <p>
             SUMMER SALE FOR ALL SWIM SUITS AND FREE EXPRESS INTERNATIONAL
             DELIVERY - OFF 50%!
-            <a href="shop.html">SHOP NOW</a>
+            <a href="shop.html"> SHOP NOW</a>
           </p>
         </div>
       </div>
