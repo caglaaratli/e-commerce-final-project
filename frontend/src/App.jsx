2e2384b4883bf6d2413d0403c73214e8bc3fc806
Cagla aratli <caglaratlii@gmail.com>
2024-02-28 15:40:19 +0300

    created header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css'
-
+import  Header from './components/Header'
 function App() {
-
-
   return (
     <>
-      <h1>HELLO</h1>
+      <Header />
     </>
   )
 }
-
-export default App
+export default App;
